9414a57107508993c15c55ece7764a74130a0a8b
OlhaBorovyk <borovyk.olga@gmail.com>
2018-02-21 19:33:42 +0200

    hub and node

--- src/main/java/com/google/core/BrowserEnum.java
+++ src/main/java/com/google/core/BrowserEnum.java
@@ -1,5 +1,5 @@
 package com.google.core;
 
 public enum BrowserEnum {
-    CHROME, FIREFOX
+    CHROME, FIREFOX, REMOTE
 }
--- src/main/java/com/google/core/WebDriverTestBase.java
+++ src/main/java/com/google/core/WebDriverTestBase.java
@@ -1,12 +1,19 @@
 package com.google.core;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 @Listeners(com.google.core.TestListener.class)
@@ -14,8 +21,18 @@ public class WebDriverTestBase {
     protected WebDriver driver;
     protected BrowserEnum browser = BrowserEnum.valueOf(System.getProperty("browser", "CHROME"));
 
+    private DesiredCapabilities setDesiredCapabilities(String platform, String remoteBrowser) {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        if (platform.equalsIgnoreCase(Platform.WIN8_1.name())) {
+            caps.setPlatform(Platform.WIN8_1);
+            caps.setBrowserName(remoteBrowser);
+        }
+        return caps;
+    }
+
+    @Parameters({"platform", "remoteBrowser"})
     @BeforeClass
-    public void SetUp() {
+    public void SetUp(@Optional String platform, @Optional String remoteBrowser) throws MalformedURLException {
         switch (browser) {
             case CHROME:
                 //WebDriverManager.chromedriver().version("2.35").setup(); //with specified version of driver
@@ -27,6 +44,11 @@ public class WebDriverTestBase {
                 WebDriverManager.firefoxdriver().setup();  //with latest version of driver
                 driver = new FirefoxDriver();
                 break;
+            case REMOTE:
+                DesiredCapabilities caps = setDesiredCapabilities(platform, remoteBrowser);
+                driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), caps);
+                break;
+
         }
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
